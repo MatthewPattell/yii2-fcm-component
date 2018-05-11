55598699b0e06ea9ec123ccd048a72105b176f82
Mikhail Yarmoluk <m.yarmoluk@kakadu.bz>
2018-05-11 15:03:41 +0300

    Init

---
+++ FcmComponent.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yarmaliuk Mikhail
+ * Date: 25.01.2018
+ * Time: 21:07
+ */
+
+namespace MP\Fcm;
+
+use paragraph1\phpFCM\Message;
+use paragraph1\phpFCM\Recipient\Device;
+use paragraph1\phpFCM\Recipient\Topic;
+use Psr\Http\Message\ResponseInterface;
+use understeam\fcm\Client;
+
+/**
+ * Class    FcmComponent
+ * @package MP\Fcm
+ * @author  Yarmaliuk Mikhail
+ * @version 1.0
+ */
+class FcmComponent extends Client
+{
+    const NOTIFICATION_DEFAULT = [
+        'key'        => 'default',
+        'title'      => '',
+        'body'       => '',
+        'badge'      => 0,
+        'priority'   => Message::PRIORITY_NORMAL,
+        'timeToLife' => '300s',
+    ];
+
+    const TARGET_TOPIC  = 'topic';
+    const TARGET_DEVICE = 'device';
+
+    /**
+     * Send push notification to topic
+     *
+     * @param array $message
+     * @param array $notification
+     *
+     * @return ResponseInterface
+     */
+    public function pushTopic(array $message, array $notification = self::NOTIFICATION_DEFAULT): ResponseInterface
+    {
+        return $this->sendPush($message, $notification, self::TARGET_TOPIC);
+    }
+
+    /**
+     * Send push notification to device
+     *
+     * @param array $message
+     * @param array $notification
+     *
+     * @return ResponseInterface
+     */
+    public function pushDevice(array $message, array $notification = self::NOTIFICATION_DEFAULT): ResponseInterface
+    {
+        return $this->sendPush($message, $notification, self::TARGET_DEVICE);
+    }
+
+    /**
+     * Send push notifications
+     *
+     * @param array  $message
+     * @param array  $notification
+     * @param string $type
+     *
+     * @return ResponseInterface
+     */
+    private function sendPush(array $message, array $notification = [], string $type): ResponseInterface
+    {
+        $notification = array_merge(self::NOTIFICATION_DEFAULT, $notification);
+
+        $note = $this->createNotification($notification['title'], $notification['body']);
+
+        if (!empty($notification['color'])) {
+            $note->setColor($notification['color']);
+        }
+
+        if (!empty($notification['icon'])) {
+            $note->setIcon($notification['icon']);
+        }
+
+        $note->setBadge($notification['badge']);
+
+        $target = NULL;
+
+        switch ($type) {
+            case self::TARGET_TOPIC:
+                $target = new Topic($message['topic']);
+            break;
+
+            case self::TARGET_DEVICE:
+                $target = new Device($message['token']);
+            break;
+        }
+
+        $msg = $this->createMessage();
+        $msg->addRecipient($target)
+            ->setNotification($note)
+            ->setPriority($notification['priority'])
+            ->setData($message['data'] ?? []);
+
+        if ($collapseKey = $message['key'] ?? $notification['key']) {
+            $msg->setCollapseKey($message['key'] ?? $notification['key']);
+        }
+
+        return $this->send($msg);
+    }
+}
---
+++ README.md
@@ -0,0 +1,70 @@
+Firebase cloud messaging for Yii2
+===========================
+Very simple FCM component for yii2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist matthew-p/yii2-fcm-component "@dev"
+```
+
+or add
+
+```
+"matthew-p/yii2-fcm-component": "@dev"
+```
+
+to the require section of your `composer.json` file.
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by:
+
+In config main.php:
+```php
+...
+'components' => [
+    ...
+    'fcm' => [
+        'class'  => \MP\Fcm\FcmComponent::class,
+        'apiKey' => 'sampleKey',
+    ],
+    ...
+],
+...
+```
+
+Use:
+```php
+// Send to topic
+$message = [
+    'topic' => 'sampleChannelID',
+    'data'  => [
+        'sample1' => 'test'
+    ],
+];
+
+$notification = ['key' => 'samplePushMessageKey'];
+
+
+Yii::$app->fcm->pushTopic($message, $notification);
+
+// Send to device
+$message = [
+    'device' => 'sampleDeviceToken',
+    'data'   => [
+        'sample1' => 'test'
+    ],
+];
+Yii::$app->fcm->pushDevice($message, $notification);
+```
+
+See $notification configuration in \MP\Fcm\FcmComponent::NOTIFICATION_DEFAULT
+
+That's all. Check it.
